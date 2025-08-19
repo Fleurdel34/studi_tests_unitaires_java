58c5f9bf242ef359a2cbe23b78c3eb5f20a50826
Fleurdel34 <betdevelweb0953@gmail.com>
2025-08-19 17:10:16 +0200

    first commit

---
+++ src/test/java/studi/test/app/SmokeTest.java
@@ -0,0 +1,13 @@
+package studi.test.app;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SmokeTest {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
